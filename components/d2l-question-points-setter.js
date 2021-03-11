2242ca8f5c546ed4882f329829f8e9bc8bc64675
Joseph Wong <joseph.wong@d2l.com>
2021-03-11 16:17:52 -0500

    components!

---
+++ components/d2l-question-points-setter.js
@@ -0,0 +1,11 @@
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+
+class QuestionPointsSetter extends LitElement {
+	static get properties() {
+		return {
+		};
+	}
+	render() {
+		return html``;
+	}
+}
